1aaa4cfdce82eaef0e4bd8d8410c4452587fe2ae
cfehlig <cassandrafehlig10@gmail.com>
2015-10-21 09:10:06 -0600

    Added a return type

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,13 +1,17 @@
 package chat.controller;
 
+import chat.view.ChatView;
+
 /**
  * Application controller for the Chatbot project.
  * @author cfeh7454
- *@version 1. 10/21/15
+ *@version 1.11 10/21/15 fixed an error
  */
 
 public class ChatController 
 {
+	private ChatView myDisplay;
+	
 	public void start()
 	{
 		
---
+++ src/chat/view/ChatView.java
@@ -0,0 +1,13 @@
+package chat.view;
+
+import javax.swing.JOptionPane;
+
+public class ChatView 
+{
+	public String grabInput(String displayText)
+	{
+		String returnedText = "";
+		
+		return returnedText;
+	}
+}
