56d6f2ea7c028ed4de967350aa30bc9e316f69ee
cfehlig <cassandrafehlig10@gmail.com>
2015-10-21 08:32:06 -0600

    Creating a Project.

---
+++ src/chat/controller/ChatController.java
@@ -0,0 +1,16 @@
+package chat.controller;
+
+/**
+ * Application controller for the Chatbot project.
+ * @author cfeh7454
+ *@version 1. 10/21/15
+ */
+
+public class ChatController 
+{
+	public void start()
+	{
+		
+	}
+
+}
---
+++ src/chat/controller/ChatRunner.java
@@ -0,0 +1,17 @@
+package chat.controller;
+/**
+ * Starts the program
+ * @author cfeh7454
+ * @version 1.0 10/21/15
+ *
+ */
+
+public class ChatRunner 
+{
+	public static void main(String[] args)
+	{
+		ChatController myChatController = new ChatController();
+		myChatController.start();
+	}
+
+}
